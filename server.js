b143ce38b470843ea20a75581bdd0b25f6033d24
Martiynz <143968800+Martiynz@users.noreply.github.com>
2025-11-10 20:32:11 -0600

    feat: agregar endpoint GET /categorias

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/', (req, res)=>{
     res.send('Hola Mundo...');
 });
 
+app.get('/categorias', (req, res)=>{
+    const categorias = [
+        {"id": 1, "nombre": "Electrónica"},
+        {"id": 2, "nombre": "Ropa"},
+        {"id": 3, "nombre": "Hogar"},
+        {"id": 4, "nombre": "Deportes"},
+        {"id": 5, "nombre": "Libros"}
+    ];
+    res.json(categorias);
+});
+
 app.listen(PORT, ()=>{
     		console.log(`Servidor escuchando en http://localhost:${PORT}`);
 	});
